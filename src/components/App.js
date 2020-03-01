a4c2f22bd7923d6d6d09e8a53569c072023e1350
Fazzacoot <farongottlieb@gmail.com>
2020-03-01 13:19:12 +0200

    S24 Replace Redux with Context

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,19 @@
 import React, { Component } from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../context/LanguageContext";
+import { LanguageStore } from "../context/LanguageContext";
 import ColorContext from "../context/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends Component {
-  state = { language: "english" };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
   render() {
     return (
       <div className="ui component">
-        <div>
-          Select a language
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange("english")}
-          ></i>
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChange("dutch")}
-          ></i>
-        </div>
-        <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector/>
           <ColorContext.Provider value="primary">
             <UserCreate />
           </ColorContext.Provider>
-        </LanguageContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
